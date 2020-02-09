368e7d4e229ebb9a126478730f8e8cfa28831b1a
Company <893042011@qq.com>
2020-02-09 12:52:55 +0800

    CORS跨域

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
@@ -15,8 +15,15 @@ export default {
   },
   data() {
     return {
-      age:20
+      age: 20
     }
+  },
+  mounted() {
+    let url =
+      'https://easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list'
+    axios.get(url).then(val => {
+      console.log(val)
+    })
   }
 }
 </script>
