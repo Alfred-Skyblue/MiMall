42cb12b51c7e50d27ace1c42a7531841c0a9aa45
Company <893042011@qq.com>
2020-02-10 16:49:31 +0800

    fulfill footer

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="">
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="javascript:;">远方的首页</a><span>|</span>
+      <a href="javascript:;">Vue全家桶</a><span>|</span>
+      <a href="javascript:;">javascript基础</a><span>|</span>
+      <a href="javascript:;">nodejs</a>
+    </div>
+    <div class="copyright">Copyright © 2018 <span class="daomai">mi.51purse.com</span> All Rights Reserved</div>
   </div>
 </template>
 
@@ -10,6 +20,37 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
-
+<style lang="scss" scoped>
+.footer{
+  height: 234px;
+  border-top: 4px solid #f60;
+  background-color: #333;
+  color: #999;
+  font-size: 16px;
+  text-align: center;
+  .footer-logo{
+    margin: 46px 0 31px 0;
+    img{
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px;
+    }
+  }
+  .footer-link{
+    margin-bottom: 13px;
+    a{
+      color: #999;
+      display: inline-block;
+    }
+    span{
+      margin: 0 10px;
+    }
+  }
+  .copyright{
+    .daomai{
+      color: #f60;
+      cursor: pointer;
+    }
+  }
+}
 </style>
---
+++ src/components/ServiceBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="service">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'service-bar'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
