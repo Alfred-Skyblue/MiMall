b69124261c20c62fa9b94e35768ffc106f1478a6
Company <893042011@qq.com>
2020-02-10 17:22:52 +0800

    fulfill

--- src/components/ServiceBar.vue
+++ src/components/ServiceBar.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <div class="service">
-      
+      <div class="container">
+        <ul>
+          <li><span class="icon-setting"></span>预约维修服务</li>
+          <li><span class="icon-7day"></span>7天无理由退货</li>
+          <li><span class="icon-15day"></span>15天免费维修</li>
+          <li><span class="icon-post"></span>满150包邮</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -13,5 +20,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import './../assets/scss/mixin.scss';
+.service{
+  margin: 0 auto;
+  width: 1226px;
+  padding: 33px 0;
+  color: #666;
+  font-size: 16px;
+  li{
+    display: inline-block;
+    width: 24.9%;
+    text-align: center;
+    border-right: 1px solid #e5e5e5;
+    span {
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      vertical-align: middle;
+      margin-right: 8px;
+    }
+    .icon-setting{
+      @include bgImg(20px,20px,'/imgs/icon-setting.png')
+    }
+    .icon-7day{
+      @include bgImg(20px,20px,'/imgs/icon-7day.png')
+    }
+    .icon-15day{
+      @include bgImg(20px,20px,'/imgs/icon-15day.png')
+    }
+    .icon-post{
+      @include bgImg(20px,20px,'/imgs/icon-post.png')
+    }
+  }
 
+}
 </style>
