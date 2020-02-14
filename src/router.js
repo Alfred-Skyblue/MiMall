7d7f63091c0dabe8bafab78396440a4b92aa45fb
Company <893042011@qq.com>
2020-02-14 21:13:47 +0800

    配置config

--- src/router.js
+++ src/router.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './pages/home'
 import Index from './pages/index'
-import Product from './pages/product'
-import Detail from './pages/detail'
-import Cart from './pages/cart'
-import Login from './pages/login'
-import Order from './pages/order'
-import OrderConfirm from './pages/orderConfirm'
-import OrderList from './pages/orderList'
+// import Product from './pages/product'
+// import Detail from './pages/detail'
+// import Cart from './pages/cart'
+// import Login from './pages/login'
+// import Order from './pages/order'
+// import OrderConfirm from './pages/orderConfirm'
+// import OrderList from './pages/orderList'
 import orderPay from './pages/orderPay'
 import Alipay from './pages/alipay'
 
@@ -30,39 +30,39 @@ export default new Router({
         {
           path: 'product/:id',
           name: 'product',
-          component: Product
+          component: resolve => require(['./pages/index.vue'], resolve)
         },
         {
           path: 'detail/:id',
           name: 'detail',
-          component: Detail
+          component: resolve => require(['./pages/detail.vue'], resolve)
         }
       ]
     },
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: () => import('./pages/login.vue')
     },
     {
       path: '/cart',
       name: 'cart',
-      component: Cart
+      component: () => import('./pages/cart.vue')
     },
     {
       path: '/order',
       name: 'order',
-      component: Order,
+      component: () => import('./pages/order.vue'),
       children: [
         {
           path: 'list',
           name: 'order-list',
-          component: OrderList
+          component: () => import('./pages/orderList.vue')
         },
         {
           path: 'confirm',
           name: 'order-confirm',
-          component: OrderConfirm
+          component: () => import('./pages/orderConfirm.vue')
         },
         {
           path: 'pay',
